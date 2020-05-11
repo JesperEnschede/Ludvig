f8b5e839c167be361e75f9d653c0de88fac1f589
JesperEnschede <jesperenschede@gmail.com>
2020-05-11 17:02:36 +0200

    +Main application class

---
+++ source/core/Application.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Jesper on 5/11/2020.
+//
+
+#include "Application.h"
+
+Ludvig::Core::Application::Application()
+{
+    // init scene.
+    // init window.
+    // init renderer.
+}
+
+void Ludvig::Core::Application::runtime()
+{
+    // while
+
+    // update renderer.
+
+    // end while
+}
---
+++ source/core/Application.h
@@ -0,0 +1,24 @@
+//
+// Created by Jesper on 5/11/2020.
+//
+
+#ifndef LUDVIG_APPLICATION_H
+#define LUDVIG_APPLICATION_H
+
+namespace Ludvig
+{
+    namespace Core
+    {
+        class Application
+        {
+        public:
+            Application();
+
+        private:
+            void runtime();
+        };
+    }
+}
+
+
+#endif //LUDVIG_APPLICATION_H
