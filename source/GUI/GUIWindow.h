658a0f9fb77293392aff859df2f256a277d2b7f0
JesperEnschede <jesperenschede@gmail.com>
2020-05-11 23:04:49 +0200

    +ongui function & window properties

--- source/GUI/GUIWindow.h
+++ source/GUI/GUIWindow.h
@@ -5,17 +5,23 @@
 #ifndef LUDVIG_GUIWINDOW_H
 #define LUDVIG_GUIWINDOW_H
 
+#include "imgui.h"
+
 namespace Ludvig
 {
     namespace GUI
     {
         class GUIWindow
         {
-            
+        public:
+            virtual void on_gui() = 0;
+
+        public:
+            const char* windowName;
+
+            ImGuiWindowFlags windowFlags;
         };
     }
 }
-
-
 
 #endif //LUDVIG_GUIWINDOW_H
