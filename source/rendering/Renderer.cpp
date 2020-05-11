9c9330b526aaee3439196d3fb1aef8a7de9ba64c
JesperEnschede <jesperenschede@gmail.com>
2020-05-11 19:29:13 +0200

    +Main openGL renderer class

---
+++ source/rendering/Renderer.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Jesper on 5/11/2020.
+//
+
+#include "Renderer.h"
+
+Ludvig::Rendering::Renderer::Renderer()
+{
+
+}
+
+void Ludvig::Rendering::Renderer::render_scene(Ludvig::Core::Scene::Scene *scene)
+{
+
+}
+
+void Ludvig::Rendering::Renderer::create_gui_frame()
+{
+
+}
+
+void Ludvig::Rendering::Renderer::draw_gui_frame()
+{
+
+}
---
+++ source/rendering/Renderer.h
@@ -0,0 +1,33 @@
+//
+// Created by Jesper on 5/11/2020.
+//
+
+#ifndef LUDVIG_RENDERER_H
+#define LUDVIG_RENDERER_H
+
+#include "../core/scene/Scene.h"
+
+namespace Ludvig
+{
+    namespace Rendering
+    {
+        class Renderer
+        {
+        public:
+            /*
+             * Loads openGL.
+             */
+            Renderer();
+
+        public:
+            void render_scene(Core::Scene::Scene* scene);
+
+            void create_gui_frame();
+            void draw_gui_frame();
+        };
+    }
+}
+
+
+
+#endif //LUDVIG_RENDERER_H
