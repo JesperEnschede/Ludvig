17ef548915f1488391ef21f04eee5584d5a78f56
JesperEnschede <jesperenschede@gmail.com>
2020-05-12 00:49:41 +0200

    -Removed test mesh creation.

--- source/core/scene/Scene.cpp
+++ source/core/scene/Scene.cpp
@@ -6,19 +6,12 @@
 
 Ludvig::Core::Scene::Scene::Scene()
 {
-    load_mesh("test");
+
 }
 
 bool Ludvig::Core::Scene::Scene::load_mesh(const char *path)
 {
-    // todo: load mesh
-    // todo: create_unique mesh instance based on the loaded mesh
-    // todo: return true|false
-
     this->meshes.push_back(std::make_unique<Mesh>());
-    this->meshes.push_back(std::make_unique<Mesh>());
-    this->meshes.push_back(std::make_unique<Mesh>());
-
 
     return false;
 }
