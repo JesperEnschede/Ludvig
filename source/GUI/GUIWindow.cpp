70fc1f7490d67d797fbe911530c401cbf3a05c13
JesperEnschede <jesperenschede@gmail.com>
2020-05-11 22:40:50 +0200

    +GUIWindow

---
+++ source/GUI/GUIWindow.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jesper on 5/11/2020.
+//
+
+#include "GUIWindow.h"
---
+++ source/GUI/GUIWindow.h
@@ -0,0 +1,21 @@
+//
+// Created by Jesper on 5/11/2020.
+//
+
+#ifndef LUDVIG_GUIWINDOW_H
+#define LUDVIG_GUIWINDOW_H
+
+namespace Ludvig
+{
+    namespace GUI
+    {
+        class GUIWindow
+        {
+            
+        };
+    }
+}
+
+
+
+#endif //LUDVIG_GUIWINDOW_H
