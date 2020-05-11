9fb475095b4dfb8157b4234fc648d7a477510f8f
JesperEnschede <jesperenschede@gmail.com>
2020-05-11 23:04:37 +0200

    +draw windows function & list of windows

--- source/GUI/GUIManager.cpp
+++ source/GUI/GUIManager.cpp
@@ -3,3 +3,23 @@
 //
 
 #include "GUIManager.h"
+#include "imgui.h"
+
+Ludvig::GUI::GUIManager::GUIManager()
+{
+
+}
+
+void Ludvig::GUI::GUIManager::draw_windows()
+{
+    for (int i = 0; i < this->windows.size(); ++i)
+    {
+        GUIWindow* window = this->windows[i].get();
+
+        ImGui::Begin(window->windowName, nullptr,window->windowFlags);
+
+        window->on_gui();
+
+        ImGui::End();
+    }
+}
--- source/GUI/GUIManager.h
+++ source/GUI/GUIManager.h
@@ -5,17 +5,27 @@
 #ifndef LUDVIG_GUIMANAGER_H
 #define LUDVIG_GUIMANAGER_H
 
+#include "GUIWindow.h"
+
+#include "memory"
+#include "vector"
+
 namespace Ludvig
 {
     namespace GUI
     {
         class GUIManager
         {
+        public:
+            GUIManager();
 
+        public:
+            void draw_windows();
+
+        private:
+            std::vector<std::unique_ptr<GUIWindow>> windows;
         };
     }
 }
-
-
 
 #endif //LUDVIG_GUIMANAGER_H
