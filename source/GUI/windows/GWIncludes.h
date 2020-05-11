a1ec0f61c96719d680f4cafe84bf1fb9eb98a615
JesperEnschede <jesperenschede@gmail.com>
2020-05-11 23:40:17 +0200

    +Global gui window includes header

---
+++ source/GUI/windows/GWIncludes.h
@@ -0,0 +1,11 @@
+//
+// Created by Jesper on 5/11/2020.
+//
+
+#ifndef LUDVIG_GWINCLUDES_H
+#define LUDVIG_GWINCLUDES_H
+
+#include "GWMenuBar.h"
+#include "GWProfiler.h"
+
+#endif //LUDVIG_GWINCLUDES_H
