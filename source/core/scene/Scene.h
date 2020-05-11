18ff222ef54ccfe1b5066d8c4b62027567db3c69
JesperEnschede <jesperenschede@gmail.com>
2020-05-11 18:07:43 +0200

    +Scene class

---
+++ source/core/scene/Scene.h
@@ -0,0 +1,25 @@
+//
+// Created by Jesper on 5/11/2020.
+//
+
+#ifndef LUDVIG_SCENE_H
+#define LUDVIG_SCENE_H
+
+namespace Ludvig
+{
+    namespace Core
+    {
+        namespace Scene
+        {
+            class Scene
+            {
+            public:
+                Scene();
+            };
+        }
+    }
+}
+
+
+
+#endif //LUDVIG_SCENE_H
