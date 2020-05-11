7fcdeffd22156558fb576e09f107ed9c66ca91be
JesperEnschede <jesperenschede@gmail.com>
2020-05-11 22:38:07 +0200

    +GUIManager

---
+++ source/GUI/GUIManager.h
@@ -0,0 +1,21 @@
+//
+// Created by Jesper on 5/11/2020.
+//
+
+#ifndef LUDVIG_GUIMANAGER_H
+#define LUDVIG_GUIMANAGER_H
+
+namespace Ludvig
+{
+    namespace GUI
+    {
+        class GUIManager
+        {
+
+        };
+    }
+}
+
+
+
+#endif //LUDVIG_GUIMANAGER_H
