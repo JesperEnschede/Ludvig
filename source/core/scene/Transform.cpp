67a4bb752fc0ee36ec429ef892ba1d372b5ca3d7
JesperEnschede <jesperenschede@gmail.com>
2020-05-12 13:13:15 +0200

    +Transform functions

--- source/core/scene/Transform.cpp
+++ source/core/scene/Transform.cpp
@@ -3,3 +3,42 @@
 //
 
 #include "Transform.h"
+
+#include "gtc/matrix_transform.hpp"
+
+Ludvig::Core::Scene::Transform::Transform()
+{
+
+}
+
+glm::mat4 Ludvig::Core::Scene::Transform::get_trs()
+{
+    glm::mat4 trs;
+
+    trs = glm::scale(trs, this->scale);
+    trs = glm::rotate(trs,glm::radians(-180.0f),this->rotation);
+    trs = glm::translate(trs,this->position);
+
+    return trs;
+}
+
+void Ludvig::Core::Scene::Transform::translate(float x, float y, float z)
+{
+    this->position.x += x;
+    this->position.y += y;
+    this->position.z += z;
+}
+
+void Ludvig::Core::Scene::Transform::rotate(float yaw, float pitch, float roll)
+{
+    this->rotation.x += yaw;
+    this->rotation.y += pitch;
+    this->rotation.z += roll;
+}
+
+void Ludvig::Core::Scene::Transform::set_scale(float x, float y, float z)
+{
+    this->scale.x += x;
+    this->scale.y += y;
+    this->scale.z += z;
+}
--- source/core/scene/Transform.h
+++ source/core/scene/Transform.h
@@ -5,10 +5,35 @@
 #ifndef LUDVIG_TRANSFORM_H
 #define LUDVIG_TRANSFORM_H
 
+#include "glm.hpp"
 
-class Transform {
+namespace Ludvig
+{
+    namespace Core
+    {
+        namespace Scene
+        {
+            class Transform
+            {
+            public:
+                Transform();
 
-};
+            public:
+                glm::mat4 get_trs();
+
+                void translate(float x, float y, float z);
+                void rotate(float yaw, float pitch, float roll);
+                void set_scale(float x, float y, float z);
+
+            private:
+                glm::vec3 position = glm::vec3(0.0f,0.0f,0.0f);
+                glm::vec3 rotation = glm::vec3(0.0f,0.0f,0.0f); // todo: quaternions.
+                glm::vec3 scale = glm::vec3(0.0f,0.0f,0.0f);
+            };
+        }
+    }
+}
+
 
 
 #endif //LUDVIG_TRANSFORM_H
