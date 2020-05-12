d9e835efa3022fde67dde72284c299dd654dca37
JesperEnschede <jesperenschede@gmail.com>
2020-05-12 12:58:53 +0200

    +Transform class

---
+++ source/core/scene/Transform.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jesper on 5/12/2020.
+//
+
+#include "Transform.h"
---
+++ source/core/scene/Transform.h
@@ -0,0 +1,14 @@
+//
+// Created by Jesper on 5/12/2020.
+//
+
+#ifndef LUDVIG_TRANSFORM_H
+#define LUDVIG_TRANSFORM_H
+
+
+class Transform {
+
+};
+
+
+#endif //LUDVIG_TRANSFORM_H
