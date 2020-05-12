718f08dfcdc0e0aa2a643992b3f053b7c1be6210
JesperEnschede <jesperenschede@gmail.com>
2020-05-12 13:19:34 +0200

    +View & projection matrix calculation

--- source/core/scene/Camera.cpp
+++ source/core/scene/Camera.cpp
@@ -3,3 +3,16 @@
 //
 
 #include "Camera.h"
+
+Ludvig::Core::Scene::Camera::Camera()
+{
+
+}
+
+glm::mat4 Ludvig::Core::Scene::Camera::get_view_projection_matrix()
+{
+    glm::mat4 view = this->transform->get_trs();
+    glm::mat4 projection = glm::perspective(glm::radians(60.0f),16.0f / 9.0f, nearClipping,farClipping);
+
+    return projection * view;
+}
--- source/core/scene/Camera.h
+++ source/core/scene/Camera.h
@@ -5,10 +5,35 @@
 #ifndef LUDVIG_CAMERA_H
 #define LUDVIG_CAMERA_H
 
+#include "Transform.h"
 
-class Camera {
+#include "glm.hpp"
+#include "gtc/matrix_transform.hpp"
 
-};
+#include "memory"
+
+namespace Ludvig
+{
+    namespace Core
+    {
+        namespace Scene
+        {
+            class Camera
+            {
+            public:
+                Camera();
+
+                glm::mat4 get_view_projection_matrix();
+
+            public:
+                std::unique_ptr<Transform> transform;
+
+                float nearClipping = 0.5f;
+                float farClipping = 100.0f;
+            };
+        }
+    }
+}
 
 
 #endif //LUDVIG_CAMERA_H
