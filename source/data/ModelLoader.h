693003d68ae922e900abefd5af3089b4ce2db236
JesperEnschede <jesperenschede@gmail.com>
2020-05-14 17:18:49 +0200

    +Model loader

--- source/data/ModelLoader.h
+++ source/data/ModelLoader.h
@@ -5,10 +5,15 @@
 #ifndef LUDVIG_MODELLOADER_H
 #define LUDVIG_MODELLOADER_H
 
+#include "glm.hpp"
+#include "vector"
 
-class ModelLoader {
-
-};
-
+namespace Ludvig
+{
+    namespace Data
+    {
+        void load_obj(const char* path, std::vector<glm::vec3>& out_vertices, std::vector<glm::vec2>& out_uvs, std::vector<glm::vec3> out_normals);
+    }
+}
 
 #endif //LUDVIG_MODELLOADER_H
