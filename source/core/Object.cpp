587199b701300edcae69c526de90b42557fc7497
JesperEnschede <jesperenschede@gmail.com>
2020-05-19 21:05:20 +0200

    +Object base class

---
+++ source/core/Object.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jesper on 5/19/2020.
+//
+
+#include "Object.h"
---
+++ source/core/Object.h
@@ -0,0 +1,14 @@
+//
+// Created by Jesper on 5/19/2020.
+//
+
+#ifndef LUDVIG_OBJECT_H
+#define LUDVIG_OBJECT_H
+
+
+class Object {
+
+};
+
+
+#endif //LUDVIG_OBJECT_H
