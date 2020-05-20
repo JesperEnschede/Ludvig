d29fc4aa7ca1161b7904bbd2225f57ffa52b46d0
JesperEnschede <jesperenschede@gmail.com>
2020-05-20 15:59:42 +0200

    *Moved files for cleanup

--- source/core/Transform.h
+++ source/core/Transform.h
@@ -31,7 +31,7 @@ namespace Ludvig
 
             private:
                 glm::vec3 position = glm::vec3(0.0f,0.0f,0.0f);
-                glm::vec3 rotation = glm::vec3(0.0f,90.0f,0.0f); // todo: quaternions.
+                glm::vec3 rotation = glm::vec3(0.0f,0.1f,0.0f); // todo: quaternions. and fix rotation in general.
                 glm::vec3 scale = glm::vec3(1.0f,1.0f,1.0f);
             };
         }
