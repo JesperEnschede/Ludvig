dd5f440ac63ac7bb0b5a71728762c79047fd5f90
JesperEnschede <jesperenschede@gmail.com>
2020-05-20 23:17:39 +0200

    +Skybox

---
+++ source/rendering/Skybox.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jesper on 5/20/2020.
+//
+
+#include "Skybox.h"
---
+++ source/rendering/Skybox.h
@@ -0,0 +1,14 @@
+//
+// Created by Jesper on 5/20/2020.
+//
+
+#ifndef LUDVIG_SKYBOX_H
+#define LUDVIG_SKYBOX_H
+
+
+class Skybox {
+
+};
+
+
+#endif //LUDVIG_SKYBOX_H
