1a825e6918340be24b0f18696fe941d8c0c2fe05
JesperEnschede <jesperenschede@gmail.com>
2020-05-21 00:00:20 +0200

    +Cubemap loading & binding

--- source/rendering/Skybox.h
+++ source/rendering/Skybox.h
@@ -5,10 +5,25 @@
 #ifndef LUDVIG_SKYBOX_H
 #define LUDVIG_SKYBOX_H
 
+#include "glad/glad.h"
 
-class Skybox {
+#include "vector"
 
-};
+namespace Ludvig
+{
+    namespace Rendering
+    {
+        class Skybox
+        {
+        public:
+            Skybox(std::vector<const char*> facePaths);
+
+        public:
+            GLuint id;
+        };
+    }
+}
+
 
 
 #endif //LUDVIG_SKYBOX_H
