3a95ebdf049aeaacd9ec3cad7526d2afe1528a30
JesperEnschede <jesperenschede@gmail.com>
2020-05-21 22:54:10 +0200

    +Basic movement for debuging.

--- source/core/Application.cpp
+++ source/core/Application.cpp
@@ -27,6 +27,32 @@ void Ludvig::Core::Application::runtime()
 
         this->window->poll_events();
 
+        // Temp input system for camera movement.
+        float forwardInput = -glfwGetKey(window->get_context(),GLFW_KEY_W);
+        float backwardsInput = glfwGetKey(window->get_context(),GLFW_KEY_S);
+        float rightInput = glfwGetKey(window->get_context(),GLFW_KEY_D);
+        float leftInput = -glfwGetKey(window->get_context(),GLFW_KEY_A);
+        float upInput = -glfwGetKey(window->get_context(),GLFW_KEY_SPACE);
+        float downInput = glfwGetKey(window->get_context(),GLFW_KEY_LEFT_SHIFT);
+
+        float yawPlusInput = -glfwGetKey(window->get_context(),GLFW_KEY_R);
+        float yawMinusInput = glfwGetKey(window->get_context(),GLFW_KEY_F);
+        float pitchPlusInput = glfwGetKey(window->get_context(),GLFW_KEY_T);
+        float pitchMinusInput = -glfwGetKey(window->get_context(),GLFW_KEY_G);
+        float rollPlusInput = -glfwGetKey(window->get_context(),GLFW_KEY_Y);
+        float rollMinusInput = glfwGetKey(window->get_context(),GLFW_KEY_H);
+
+        // Temp cameras movement.
+        scene->camera->transform->translate(
+                (rightInput + leftInput) / 150,
+                (upInput + downInput) / 150,
+                (forwardInput + backwardsInput) / 150);
+
+        scene->meshes[0]->transform->set_scale(
+                (yawPlusInput + yawMinusInput) / 150,
+                (pitchPlusInput + pitchMinusInput) / 150,
+                (rollPlusInput + rollMinusInput) / 150);
+
         this->renderer->render_scene(this->scene.get());
 
         this->renderer->create_gui_frame();
@@ -34,6 +60,12 @@ void Ludvig::Core::Application::runtime()
 
         ImGui::Begin("profiler");
         ImGui::Text("%.3f ms/frame | %.3f FPS", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        ImGui::End();
+
+        ImGui::Begin("Camera");
+        ImGui::Text("Position | %.2f %.2f %.2f", scene->camera->transform->get_position().x,scene->camera->transform->get_position().y,scene->camera->transform->get_position().z);
+        ImGui::Text("Rotation | %.2f %.2f %.2f", scene->camera->transform->get_rotation().x,scene->camera->transform->get_rotation().y,scene->camera->transform->get_rotation().z);
+        ImGui::Text("Scale | %.2f %.2f %.2f", scene->camera->transform->get_scale().x,scene->camera->transform->get_scale().y,scene->camera->transform->get_scale().z);
         ImGui::End();
 
         this->renderer->draw_gui_frame();
