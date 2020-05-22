7708dcf2033e36df1c9b9e9448479baa2ab4bf30
JesperEnschede <jesperenschede@gmail.com>
2020-05-22 18:07:22 +0200

    +Profiler gui window

---
+++ source/GUI/windows/GWProfiler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jesper on 5/22/2020.
+//
+
+#include "GWProfiler.h"
---
+++ source/GUI/windows/GWProfiler.h
@@ -0,0 +1,14 @@
+//
+// Created by Jesper on 5/22/2020.
+//
+
+#ifndef LUDVIG_GWPROFILER_H
+#define LUDVIG_GWPROFILER_H
+
+
+class GWProfiler {
+
+};
+
+
+#endif //LUDVIG_GWPROFILER_H
