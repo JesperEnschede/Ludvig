bab846e02b80229ecddeb7ebca06228cedbafd41
JesperEnschede <jesperenschede@gmail.com>
2020-05-23 16:30:22 +0200

    +LightSettings unique ptr

--- source/core/scene/Scene.cpp
+++ source/core/scene/Scene.cpp
@@ -10,9 +10,9 @@ Ludvig::Core::Scene::Scene::Scene()
 {
     this->camera = std::make_unique<Camera>();
     this->light = std::make_unique<Light>();
+    this->lightSettings = std::make_unique<Rendering::LightSettings>();
 
     this->light->name = "Light";
-
     this->camera->name = "Camera";
     this->camera->transform->translate(0,0,5);
 
@@ -28,7 +28,6 @@ bool Ludvig::Core::Scene::Scene::load_mesh(const char *path)
     Data::load_obj(path, vertices,uvs,normals);
 
     this->meshes.push_back(std::make_unique<Mesh>(vertices,uvs,normals));
-    this->meshes[0]->name = "Mesh";
 
     return false;
 }
