7f6e6c691cd74f280c98639766e934ef69599e9f
JesperEnschede <jesperenschede@gmail.com>
2020-05-23 13:45:54 +0200

    +Gui object inspector window

---
+++ source/GUI/windows/GWObjectInspector.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jesper on 5/23/2020.
+//
+
+#include "GWObjectInspector.h"
---
+++ source/GUI/windows/GWObjectInspector.h
@@ -0,0 +1,14 @@
+//
+// Created by Jesper on 5/23/2020.
+//
+
+#ifndef LUDVIG_GWOBJECTINSPECTOR_H
+#define LUDVIG_GWOBJECTINSPECTOR_H
+
+
+class GWObjectInspector {
+
+};
+
+
+#endif //LUDVIG_GWOBJECTINSPECTOR_H
