d5613f01bea116e9e5417989603a710e24074c35
JesperEnschede <jesperenschede@gmail.com>
2020-05-23 14:22:43 +0200

    +Mesh inspector gui window

---
+++ source/GUI/windows/GWMeshInspector.h
@@ -0,0 +1,14 @@
+//
+// Created by Jesper on 5/23/2020.
+//
+
+#ifndef LUDVIG_GWMESHINSPECTOR_H
+#define LUDVIG_GWMESHINSPECTOR_H
+
+
+class GWMeshInspector {
+
+};
+
+
+#endif //LUDVIG_GWMESHINSPECTOR_H
