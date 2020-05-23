de9b251074d67375ce2a439220d8b07b62e5de92
JesperEnschede <jesperenschede@gmail.com>
2020-05-23 14:46:04 +0200

    +Light gui inspector

---
+++ source/GUI/windows/GWLightInspector.h
@@ -0,0 +1,14 @@
+//
+// Created by Jesper on 5/23/2020.
+//
+
+#ifndef LUDVIG_GWLIGHTINSPECTOR_H
+#define LUDVIG_GWLIGHTINSPECTOR_H
+
+
+class GWLightInspector {
+
+};
+
+
+#endif //LUDVIG_GWLIGHTINSPECTOR_H
