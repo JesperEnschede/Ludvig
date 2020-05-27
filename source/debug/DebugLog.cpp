5cb52a3935bdbccabe94c471c1143a25a64c4c69
JesperEnschede <jesperenschede@gmail.com>
2020-05-27 23:26:53 +0200

    +Message logging

--- source/debug/DebugLog.cpp
+++ source/debug/DebugLog.cpp
@@ -3,3 +3,23 @@
 //
 
 #include "DebugLog.h"
+
+Ludvig::Debug::DebugLog::DebugLog()
+{
+    instance = this;
+}
+
+void Ludvig::Debug::DebugLog::log_message(std::string message)
+{
+    instance->log += message;
+}
+
+void Ludvig::Debug::DebugLog::log_warning(std::string warning)
+{
+    instance->log += "warning: " + warning;
+}
+
+void Ludvig::Debug::DebugLog::log_error(std::string error)
+{
+    instance->log += "error: " + error;
+}
--- source/debug/DebugLog.h
+++ source/debug/DebugLog.h
@@ -5,10 +5,29 @@
 #ifndef LUDVIG_DEBUGLOG_H
 #define LUDVIG_DEBUGLOG_H
 
+#include "string"
 
-class DebugLog {
+namespace Ludvig
+{
+    namespace Debug
+    {
+        class DebugLog
+        {
+        public:
+            DebugLog();
 
-};
+            static void log_message(std::string message);
+            static void log_warning(std::string warning);
+            static void log_error(std::string error);
+
+        private:
+            static DebugLog* instance = nullptr;
+
+            std::string log = "";
+        };
+    }
+}
+
 
 
 #endif //LUDVIG_DEBUGLOG_H
