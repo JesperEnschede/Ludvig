280fe5dd5c14e7f8db2de142d93882d810c33809
JesperEnschede <jesperenschede@gmail.com>
2020-05-28 00:25:56 +0200

    *Shader set model view projection fix

--- source/rendering/Renderer.cpp
+++ source/rendering/Renderer.cpp
@@ -63,7 +63,7 @@ void Ludvig::Rendering::Renderer::render_scene(Ludvig::Core::Scene::Scene *scene
     this->shaders[0]->set_vec3("lightColor",scene->light->color);
     this->shaders[0]->set_vec3("ambientColor",scene->lightSettings->ambientLightColor);
     this->shaders[0]->set_mat4x4("V",scene->camera->get_view_matrix());
-    this->shaders[0]->set_mat4x4("MVP",mvp);
+
 
     for (int i = 0; i < scene->meshes.size(); ++i)
     {
@@ -77,6 +77,7 @@ void Ludvig::Rendering::Renderer::render_scene(Ludvig::Core::Scene::Scene *scene
         glBindTexture(GL_TEXTURE_2D, this->textures[0]->id);
         this->shaders[0]->set_texture("textureSampler",this->textures[0]->id);
         this->shaders[0]->set_mat4x4("M",mesh->transform->get_trs());
+        this->shaders[0]->set_mat4x4("MVP",mvp);
 
         //todo: fix vertex attrib array enable : disable :(
         glEnableVertexAttribArray(0);
