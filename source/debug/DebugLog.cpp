19437c50a3c991b0736988f7f91f3fa6df87f746
JesperEnschede <jesperenschede@gmail.com>
2020-05-27 22:50:47 +0200

    +DebugLog

---
+++ source/debug/DebugLog.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jesper on 5/27/2020.
+//
+
+#include "DebugLog.h"
---
+++ source/debug/DebugLog.h
@@ -0,0 +1,14 @@
+//
+// Created by Jesper on 5/27/2020.
+//
+
+#ifndef LUDVIG_DEBUGLOG_H
+#define LUDVIG_DEBUGLOG_H
+
+
+class DebugLog {
+
+};
+
+
+#endif //LUDVIG_DEBUGLOG_H
