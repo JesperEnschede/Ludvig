5ffc341bb205a34c4f614990b00638cd267ec4f2
JesperEnschede <jesperenschede@gmail.com>
2020-05-29 13:25:52 +0200

    +VertexArrayObject

---
+++ source/rendering/VertexArrayObject.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jesper on 5/29/2020.
+//
+
+#include "VertexArrayObject.h"
---
+++ source/rendering/VertexArrayObject.h
@@ -0,0 +1,14 @@
+//
+// Created by Jesper on 5/29/2020.
+//
+
+#ifndef LUDVIG_VERTEXARRAYOBJECT_H
+#define LUDVIG_VERTEXARRAYOBJECT_H
+
+
+class VertexArrayObject {
+
+};
+
+
+#endif //LUDVIG_VERTEXARRAYOBJECT_H
