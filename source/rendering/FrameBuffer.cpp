d6fb81e06a96d60262f69e14dae598d64e54155c
JesperEnschede <jesperenschede@gmail.com>
2020-05-29 17:21:57 +0200

    +FrameBuffer

---
+++ source/rendering/FrameBuffer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jesper on 5/29/2020.
+//
+
+#include "FrameBuffer.h"
---
+++ source/rendering/FrameBuffer.h
@@ -0,0 +1,14 @@
+//
+// Created by Jesper on 5/29/2020.
+//
+
+#ifndef LUDVIG_FRAMEBUFFER_H
+#define LUDVIG_FRAMEBUFFER_H
+
+
+class FrameBuffer {
+
+};
+
+
+#endif //LUDVIG_FRAMEBUFFER_H
