5b0e787ef9d2a5dabe01407b1727f37b77dfad93
JesperEnschede <jesperenschede@gmail.com>
2020-05-29 17:25:08 +0200

    +Generation & cleanup of framebuffer

--- source/rendering/FrameBuffer.cpp
+++ source/rendering/FrameBuffer.cpp
@@ -3,3 +3,20 @@
 //
 
 #include "FrameBuffer.h"
+
+#include "glad/glad.h"
+
+Ludvig::Rendering::FrameBuffer::FrameBuffer()
+{
+    glGenBuffers(1,&this->buffer);
+}
+
+Ludvig::Rendering::FrameBuffer::~FrameBuffer()
+{
+    glDeleteFramebuffers(1,&this->buffer);
+}
+
+void Ludvig::Rendering::FrameBuffer::bind(int target)
+{
+
+}
--- source/rendering/FrameBuffer.h
+++ source/rendering/FrameBuffer.h
@@ -5,10 +5,24 @@
 #ifndef LUDVIG_FRAMEBUFFER_H
 #define LUDVIG_FRAMEBUFFER_H
 
+namespace Ludvig
+{
+    namespace Rendering
+    {
+        class FrameBuffer
+        {
+        public:
+            FrameBuffer();
+            ~FrameBuffer();
 
-class FrameBuffer {
+            void bind(int target);
 
-};
+        private:
+            unsigned int buffer;
+        };
+    }
+}
+
 
 
 #endif //LUDVIG_FRAMEBUFFER_H
