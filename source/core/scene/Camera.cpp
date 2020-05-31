279f117d2dd876e860005a9b094f3eb556793b94
JesperEnschede <jesperenschede@gmail.com>
2020-05-31 16:04:12 +0200

    *Sort of fix for camera rotation

--- source/core/scene/Camera.cpp
+++ source/core/scene/Camera.cpp
@@ -11,7 +11,21 @@ glm::mat4 Ludvig::Core::Scene::Camera::get_view_projection_matrix()
 
 void Ludvig::Core::Scene::Camera::calculate_view_projection_matrix()
 {
-    this->viewMatrix = this->transform->get_trs();
+    // this->viewMatrix = this->transform->get_trs();
+
+    glm::vec3 direction
+    (
+           cos(this->transform->rotation.x) * sin(this->transform->rotation.y),
+           sin(this->transform->rotation.x),
+           cos(this->transform->rotation.x) * cos(this->transform->rotation.y)
+    );
+
+    this->viewMatrix = glm::lookAt
+            (
+                this->transform->position,
+                this->transform->position + direction,
+                glm::vec3(0,1,0)
+            );
     this->projectionMatrix = glm::perspective(glm::radians(this->fieldOfView),16.0f / 9.0f, this->nearClipping,this->farClipping);
 }
 
