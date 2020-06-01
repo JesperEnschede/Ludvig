f07e9210ca24da7da5129cd348e6a47078dc0a4a
JesperEnschede <jesperenschede@gmail.com>
2020-06-01 14:05:28 +0200

    +Material class

---
+++ source/rendering/Material.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jesper on 6/1/2020.
+//
+
+#include "Material.h"
---
+++ source/rendering/Material.h
@@ -0,0 +1,14 @@
+//
+// Created by Jesper on 6/1/2020.
+//
+
+#ifndef LUDVIG_MATERIAL_H
+#define LUDVIG_MATERIAL_H
+
+
+class Material {
+
+};
+
+
+#endif //LUDVIG_MATERIAL_H
