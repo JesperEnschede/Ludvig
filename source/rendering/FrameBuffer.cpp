32d8d849115d7a0028fa4b853be59d930a143d73
JesperEnschede <jesperenschede@gmail.com>
2020-06-02 21:50:04 +0200

    *Cleaned up framebuffer creation for actual use.

--- source/rendering/FrameBuffer.cpp
+++ source/rendering/FrameBuffer.cpp
@@ -11,20 +11,17 @@
 
 Ludvig::Rendering::FrameBuffer::FrameBuffer()
 {
-    glGenBuffers(1,&this->buffer);
-    glBindFramebuffer(GL_FRAMEBUFFER, this->buffer);
+    glGenBuffers(1,&this->frameBufferObject);
+    glBindFramebuffer(GL_FRAMEBUFFER, this->frameBufferObject);
 
-    glGenTextures(1,&this->colorBuffer);
-    glBindTexture(GL_TEXTURE_2D, this->colorBuffer);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 1920, 1080, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, this->colorBuffer, 0);
+    this->colorBufferTexture = std::make_unique<Core::Scene::Texture>();
 
-    glGenRenderbuffers(1,&this->renderBuffer);
-    glBindRenderbuffer(GL_RENDERBUFFER, this->renderBuffer);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, this->colorBufferTexture->id, 0);
+
+    glGenRenderbuffers(1,&this->renderBufferObject);
+    glBindRenderbuffer(GL_RENDERBUFFER, this->renderBufferObject);
     glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, 1920, 1080);
-    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, this->renderBuffer);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, this->renderBufferObject);
 
     if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
     {
--- source/rendering/FrameBuffer.h
+++ source/rendering/FrameBuffer.h
@@ -5,6 +5,12 @@
 #ifndef LUDVIG_FRAMEBUFFER_H
 #define LUDVIG_FRAMEBUFFER_H
 
+#include "VertexBufferObject.h"
+#include "Texture.h"
+
+#include "Shader.h"
+#include "../core/scene/Mesh.h"
+
 namespace Ludvig
 {
     namespace Rendering
@@ -18,9 +24,31 @@ namespace Ludvig
             void bind(unsigned int target);
 
         private:
-            unsigned int buffer;
-            unsigned int colorBuffer;
-            unsigned int renderBuffer;
+            unsigned int frameBufferObject;
+            unsigned int renderBufferObject;
+
+            std::unique_ptr<Core::Scene::Texture> colorBufferTexture;
+
+            Core::Scene::Mesh screenQuad =
+                    Core::Scene::Mesh
+                    (
+                            {
+                                glm::vec3(-1.0f,  1.0f,0.0f),
+                                glm::vec3(-1.0f, -1.0f,0.0f),
+                                glm::vec3(1.0f, -1.0f,0.0f),
+                                glm::vec3(-1.0f,  1.0f,0.0f),
+                                glm::vec3(1.0f, -1.0f,0.0f),
+                                glm::vec3(1.0f,  1.0f,0.0f)
+                                },
+                            {
+                                glm::vec3(0.0f, 1.0f,0.0f),
+                                glm::vec3( 0.0f, 0.0f,0.0f),
+                                glm::vec3(1.0f, 0.0f,0.0f),
+                                glm::vec3(0.0f, 1.0f,0.0f),
+                                glm::vec3(1.0f, 0.0f,0.0f),
+                                glm::vec3(1.0f, 1.0f,0.0f)
+                                },
+                            { /* we don't do normals here, because i said so :p */ });
         };
     }
 }
