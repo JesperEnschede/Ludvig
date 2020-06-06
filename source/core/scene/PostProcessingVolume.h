80bf966017e5f6ce552653bd86e8690d91b329d2
JesperEnschede <jesperenschede@gmail.com>
2020-06-06 14:03:24 +0200

    *Post processing values

--- source/core/scene/PostProcessingVolume.h
+++ source/core/scene/PostProcessingVolume.h
@@ -5,10 +5,25 @@
 #ifndef LUDVIG_POSTPROCESSINGVOLUME_H
 #define LUDVIG_POSTPROCESSINGVOLUME_H
 
+namespace Ludvig
+{
+    namespace Core
+    {
+        namespace Scene
+        {
+            class PostProcessingVolume
+            {
+            public:
+                bool enable_kernel;
+                bool enable_colorShift;
+                bool enable_invert;
 
-class PostProcessingVolume {
-
-};
+                float kernel[9] = { 0,0,0,0,0,0,0,0,0};
+                float colorShift[3] = { 1,1,1};
+            };
+        }
+    }
+}
 
 
 #endif //LUDVIG_POSTPROCESSINGVOLUME_H
