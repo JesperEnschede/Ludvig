cff535e023237d01106f374ec7bd82b27908fac2
JesperEnschede <jesperenschede@gmail.com>
2020-06-06 13:54:34 +0200

    +Post processing volume class

---
+++ source/core/scene/PostProcessingVolume.h
@@ -0,0 +1,14 @@
+//
+// Created by Jesper on 6/6/2020.
+//
+
+#ifndef LUDVIG_POSTPROCESSINGVOLUME_H
+#define LUDVIG_POSTPROCESSINGVOLUME_H
+
+
+class PostProcessingVolume {
+
+};
+
+
+#endif //LUDVIG_POSTPROCESSINGVOLUME_H
