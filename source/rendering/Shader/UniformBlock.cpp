d44d87f1a2d8a1382ce16684619f861f5a62ecdf
JesperEnschede <jesperenschede@gmail.com>
2020-06-07 22:51:52 +0200

    +Uniform block binding

--- source/rendering/Shader/UniformBlock.cpp
+++ source/rendering/Shader/UniformBlock.cpp
@@ -3,3 +3,12 @@
 //
 
 #include "UniformBlock.h"
+
+#include "glad/glad.h"
+
+Ludvig::Rendering::UniformBlock::UniformBlock(unsigned int programID, const char *blockName)
+{
+    this->uniformBlockIndex = glGetUniformBlockIndex(programID, blockName);
+
+    glUniformBlockBinding(programID, uniformBlockIndex, 0);
+}
--- source/rendering/Shader/UniformBlock.h
+++ source/rendering/Shader/UniformBlock.h
@@ -5,10 +5,21 @@
 #ifndef LUDVIG_UNIFORMBLOCK_H
 #define LUDVIG_UNIFORMBLOCK_H
 
+namespace Ludvig
+{
+    namespace Rendering
+    {
+        class UniformBlock
+        {
+        public:
+            UniformBlock(unsigned int programID, const char* blockName);
 
-class UniformBlock {
+        private:
+            unsigned int uniformBlockIndex;
+        };
+    }
+}
 
-};
 
 
 #endif //LUDVIG_UNIFORMBLOCK_H
