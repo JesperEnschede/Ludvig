cad55ebfab0a1d889a54c48c680e0877d15d2cfe
JesperEnschede <jesperenschede@gmail.com>
2020-06-07 13:19:25 +0200

    +Uniform buffer object class

---
+++ source/rendering/Buffers/UniformBufferObject.h
@@ -0,0 +1,14 @@
+//
+// Created by Jesper on 6/7/2020.
+//
+
+#ifndef LUDVIG_UNIFORMBUFFEROBJECT_H
+#define LUDVIG_UNIFORMBUFFEROBJECT_H
+
+
+class UniformBufferObject {
+
+};
+
+
+#endif //LUDVIG_UNIFORMBUFFEROBJECT_H
