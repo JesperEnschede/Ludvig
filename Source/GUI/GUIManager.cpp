6491c7224d01be5c1649b5be53cfeaf211841cad
Jesper Enschede <jesperensched@gmail.com>
2020-07-13 00:19:52 +0200

    Added the new GUIManager class

---
+++ Source/GUI/GUIManager.cpp
@@ -0,0 +1,13 @@
+//
+// File created by Jesper on 7/13/2020.
+//
+
+#include "GUIManager.h"
+
+namespace Ludvig
+{
+    namespace GUI
+    {
+
+    }
+}
---
+++ Source/GUI/GUIManager.h
@@ -0,0 +1,22 @@
+//
+// File created by Jesper on 7/13/2020.
+//
+
+#ifndef LUDVIG_GUIMANAGER_H
+#define LUDVIG_GUIMANAGER_H
+
+namespace Ludvig
+{
+    namespace GUI
+    {
+        /*
+         * The GUIManager draws the GUI windows.
+         */
+        class GUIManager final
+        {
+            
+        };
+    }
+}
+
+#endif //LUDVIG_GUIMANAGER_H
