5cbb549fe38e9292a11de7d35b7d814bc8a152d8
Jesper Enschede <jesperensched@gmail.com>
2020-08-06 16:07:42 +0200

    Added OpenGL render context class

---
+++ Source/Rendering/OpenGL/OpenGLRenderContext.h
@@ -0,0 +1,21 @@
+//
+// File created by Jesper on 8/6/2020.
+//
+
+#ifndef LUDVIG_OPENGLRENDERCONTEXT_H
+#define LUDVIG_OPENGLRENDERCONTEXT_H
+
+#include "Rendering/RenderContext.h"
+
+namespace Ludvig
+{
+    namespace Rendering
+    {
+        class OpenGLRenderContext : public RenderContext
+        {
+
+        };
+    }
+}
+
+#endif //LUDVIG_OPENGLRENDERCONTEXT_H
