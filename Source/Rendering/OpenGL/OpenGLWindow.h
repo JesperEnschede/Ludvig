11416a46c8f2da59dc7bc96bdfea55e4ac08f955
Jesper Enschede <jesperensched@gmail.com>
2020-08-06 16:15:32 +0200

    Added OpenGL window class (GLFW)

---
+++ Source/Rendering/OpenGL/OpenGLWindow.h
@@ -0,0 +1,24 @@
+//
+// File created by Jesper on 8/6/2020.
+//
+
+#ifndef LUDVIG_OPENGLWINDOW_H
+#define LUDVIG_OPENGLWINDOW_H
+
+#include "Rendering/Window.h"
+
+namespace Ludvig
+{
+    namespace Rendering
+    {
+        namespace OpenGL
+        {
+            class OpenGLWindow : public Window
+            {
+
+            };
+        }
+    }
+}
+
+#endif //LUDVIG_OPENGLWINDOW_H
