57e4dc8413ded8a03cf9dd7ffe99e88afd9dddd6
Jesper Enschede <jesperensched@gmail.com>
2020-08-07 14:10:26 +0200

    Set vertex attribute pointers

--- Source/Rendering/OpenGL/OpenGLMeshRenderer.cpp
+++ Source/Rendering/OpenGL/OpenGLMeshRenderer.cpp
@@ -8,6 +8,7 @@
 
 #include "OpenGLVertexArray.h"
 #include "OpenGLBuffer.h"
+#include "OpenGLVertexAttrib.h"
 
 #include "Debug/DebugLog.h"
 
@@ -28,9 +29,21 @@ namespace Ludvig
                  */
 
                 vao = create_vertex_array();
+
+                /*
+                 * Maybe i should create a "create buffer" function inside of the mesh renderer,
+                 * because manually incrementing the vertexattrib index is kinda dirty tbh ;)
+                 * idk, we'll see if this causes any major problems in the near future.
+                 * -Jesper 8/7/2020
+                 */
                 buffers.push_back(create_buffer(GL_ARRAY_BUFFER, mesh->vertices.data(), mesh->vertices.size(), GL_STATIC_DRAW));
+                vertex_attrib_ptr(0,3);
+
                 buffers.push_back(create_buffer(GL_ARRAY_BUFFER, mesh->uvs.data(), mesh->uvs.size(), GL_STATIC_DRAW));
+                vertex_attrib_ptr(1,2);
+
                 buffers.push_back(create_buffer(GL_ARRAY_BUFFER, mesh->normals.data(), mesh->normals.size(), GL_STATIC_DRAW));
+                vertex_attrib_ptr(2,3);
 
                 Debug::DebugLog::log_message("Created OpenGL mesh renderer");
             }
