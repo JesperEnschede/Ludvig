19884f48a5cfe64142f8e0365a5ab1cd6667d36e
Jesper Enschede <jesperensched@gmail.com>
2020-08-07 17:54:49 +0200

    Added shader base class

---
+++ Source/Rendering/Shader.cpp
@@ -0,0 +1,13 @@
+//
+// File created by Jesper on 8/7/2020.
+//
+
+#include "Shader.h"
+
+namespace Ludvig
+{
+    namespace Rendering
+    {
+
+    }
+}
---
+++ Source/Rendering/Shader.h
@@ -0,0 +1,26 @@
+//
+// File created by Jesper on 8/7/2020.
+//
+
+#ifndef LUDVIG_SHADER_H
+#define LUDVIG_SHADER_H
+
+#include "string"
+
+namespace Ludvig
+{
+    namespace Rendering
+    {
+        /*
+         * The shader class is the base class for shader programs,
+         * The class must be overridden to define API specific behaviour
+         * A shader is owned by a RenderManager.
+         */
+        class Shader
+        {
+
+        };
+    }
+}
+
+#endif //LUDVIG_SHADER_H
