a0933ce9754fcaebf712e87e0e4b6f24e3f58d6d
Jesper Enschede <jesperensched@gmail.com>
2020-08-09 13:44:59 +0200

    Changed the shader class to only have file paths to shaders (read commit description)
    The RenderManager is responsible for creating API specific

--- Source/Rendering/Shader.h
+++ Source/Rendering/Shader.h
@@ -1,7 +1,6 @@
 //
 // File created by Jesper on 8/7/2020.
 //
-
 #ifndef LUDVIG_SHADER_H
 #define LUDVIG_SHADER_H
 
@@ -12,13 +11,22 @@ namespace Ludvig
     namespace Rendering
     {
         /*
-         * The shader class is the base class for shader programs,
-         * The class must be overridden to define API specific behaviour
+         * The Shader data struct is used to store
+         * file paths of shaders, The RenderManager is then
+         * responsible for creating API specific shaders programs.
          * A shader is owned by a RenderManager.
          */
-        class Shader
+        struct Shader
         {
+        public:
+            /*
+             * TODO(Jesper) Add support for more shader types.
+             * Would be cool to add support for stuff like
+             * geometry, compute or other shaders :)
+             */
 
+            std::string vertexFilePath;
+            std::string fragmentFilePath;
         };
     }
 }
