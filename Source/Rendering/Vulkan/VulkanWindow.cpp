09c37981f9201a7e38348f84bd42e91dca44c46d
Jesper Enschede <jesperensched@gmail.com>
2020-08-10 16:03:38 +0200

    Added vulkan window class

---
+++ Source/Rendering/Vulkan/VulkanWindow.cpp
@@ -0,0 +1,18 @@
+//
+// File created by Jesper on 8/10/2020.
+//
+
+#include "VulkanWindow.h"
+
+namespace Ludvig
+{
+    namespace Rendering
+    {
+        namespace Vulkan
+        {
+            VulkanWindow::VulkanWindow() {
+
+            }
+        }
+    }
+}
---
+++ Source/Rendering/Vulkan/VulkanWindow.h
@@ -0,0 +1,25 @@
+//
+// File created by Jesper on 8/10/2020.
+//
+
+#ifndef LUDVIG_VULKANWINDOW_H
+#define LUDVIG_VULKANWINDOW_H
+
+#include "Rendering/Window.h"
+
+namespace Ludvig
+{
+    namespace Rendering
+    {
+        namespace Vulkan
+        {
+            class VulkanWindow : public Window
+            {
+            public:
+                VulkanWindow();
+            };
+        }
+    }
+}
+
+#endif //LUDVIG_VULKANWINDOW_H
