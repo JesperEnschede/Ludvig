c1e4aaaab3f0c38e35d541ced3a47e5285510a81
Jesper Enschede <jesperensched@gmail.com>
2020-08-10 15:53:58 +0200

    Added vulkan render manager class

---
+++ Source/Rendering/Vulkan/VulkanRenderManager.cpp
@@ -0,0 +1,22 @@
+//
+// File created by Jesper on 8/10/2020.
+//
+
+#include "VulkanRenderManager.h"
+
+namespace Ludvig
+{
+    namespace Rendering
+    {
+        namespace Vulkan
+        {
+            void VulkanRenderManager::render(Core::Scene *scene) {
+
+            }
+
+            void VulkanRenderManager::create_mesh_renderer(Core::Mesh *mesh) {
+
+            }
+        }
+    }
+}
---
+++ Source/Rendering/Vulkan/VulkanRenderManager.h
@@ -0,0 +1,27 @@
+//
+// File created by Jesper on 8/10/2020.
+//
+
+#ifndef LUDVIG_VULKANRENDERMANAGER_H
+#define LUDVIG_VULKANRENDERMANAGER_H
+
+#include "Rendering/RenderManager.h"
+
+namespace Ludvig
+{
+    namespace Rendering
+    {
+        namespace Vulkan
+        {
+            class VulkanRenderManager : public RenderManager
+            {
+            public:
+                void render(Core::Scene *scene) override;
+
+                void create_mesh_renderer(Core::Mesh *mesh) override;
+            };
+        }
+    }
+}
+
+#endif //LUDVIG_VULKANRENDERMANAGER_H
