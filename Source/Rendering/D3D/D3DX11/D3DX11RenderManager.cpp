34a5da7f8bd435070d7d8a4ae6ecda7191baa800
Jesper Enschede <jesperensched@gmail.com>
2020-08-21 13:32:58 +0200

    Added D3DX11 render manager

---
+++ Source/Rendering/D3D/D3DX11/D3DX11RenderManager.cpp
@@ -0,0 +1,26 @@
+//
+// File created by Jesper on 8/21/2020.
+//
+
+#include "D3DX11RenderManager.h"
+
+namespace Ludvig
+{
+    namespace Rendering
+    {
+        namespace D3DX11
+        {
+            D3DX11RenderManager::D3DX11RenderManager() {
+
+            }
+
+            void D3DX11RenderManager::render(Core::Scene *scene) {
+
+            }
+
+            void D3DX11RenderManager::create_mesh_renderer(Core::Mesh *mesh) {
+
+            }
+        }
+    }
+}
---
+++ Source/Rendering/D3D/D3DX11/D3DX11RenderManager.h
@@ -0,0 +1,29 @@
+//
+// File created by Jesper on 8/21/2020.
+//
+
+#ifndef LUDVIG_D3DX11RENDERMANAGER_H
+#define LUDVIG_D3DX11RENDERMANAGER_H
+
+#include "Rendering/RenderManager.h"
+
+namespace Ludvig
+{
+    namespace Rendering
+    {
+        namespace D3DX11
+        {
+            class D3DX11RenderManager : public RenderManager
+            {
+            public:
+                D3DX11RenderManager();
+
+                void render(Core::Scene *scene) override;
+
+                void create_mesh_renderer(Core::Mesh *mesh) override;
+            };
+        }
+    }
+}
+
+#endif //LUDVIG_D3DX11RENDERMANAGER_H
